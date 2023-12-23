71b1ba3ca539de0192faf8410e9c8a38a2c578de
Mauro Guida <Jimbus369@gmail.com>
2023-12-23 20:59:16 +0100

    New ad skip method

--- skipad.js
+++ skipad.js
@@ -1,31 +1,18 @@
 (function () {
-  var skipButtonClassNameList = [
-    'videoAdUiSkipButton', // Old close ad button
-    'ytp-ad-skip-button ytp-button', // New close ad button
-    'ytp-ad-overlay-close-button', // Close overlay button
-    "ytp-ad-skip-button-modern ytp-button", // Modern close ad button
-  ];
-  
   var timeoutId;
-  var observedSkipBtn;
-  var skipBtnObserver;
-  var isMuted = false;  
 
-  /**
-  * Loops over all the class names of buttons that we need to click to skip an
-  * ad or overlay, and returns an array of those elements.
-  *
-  * @param {Array<String>} classNames - an array of class names of buttons that we need to click
-  * @returns {Array<Element>} - An array of DOM elements
-  */
-  function existingButtons(classNames) {
-    return classNames
-      .map(name => {
-        return Array.from(document.getElementsByClassName(name)) || [];
-      })
-      .reduce(function(acc, elems) {
-        return acc.concat(elems);
-      }, [])
+  function checkAndSkipAd() {
+    var adDiv = document.querySelector('div.video-ads');
+    var adPlayer = document.querySelector('video.video-stream.html5-main-video');
+
+    if (isElementVisible(adDiv)) {
+      console.log("Ad detected");
+      
+      if (adPlayer.currentTime < 900 && !adPlayer.paused) {
+        adPlayer.currentTime = 900; //Set current video time to 15 Minutes
+        console.log("Ad skipped");
+      }
+    }
   }
 
   /**
@@ -36,124 +23,10 @@
   * @returns {boolean} - Whether the element is visible on the screen
   */
   function isElementVisible(el) {
+    if (!el)
+      return false;
+
     return el.offsetParent === null ? false : true;
-  }
-
-  /**
-  * Since we do not click the button as long as it is not visible, we can
-  * attach an observer to listen for the button's attribute changes to figure
-  * out when the element becomes visible.
-  * 
-  * @param {Element} button - The button element to click
-  */
-  function triggerClickWhenVisible(button) {
-    if (button === observedSkipBtn) {
-      // We are already observing this button.
-      return;
-    }
-
-    // Find the actual parent with the display style 'none' so that we can
-    // listen to that element's changes.
-    var parentWithDisplayStyle = (function() {
-      var currentParent = button;
-      while (currentParent !== null) {
-        if (currentParent.style.display === 'none') {
-          return currentParent;
-        }
-
-        currentParent = currentParent.parentElement;
-      }
-
-      return null;
-    })();
-
-    if (!parentWithDisplayStyle) {
-      // Give up.
-      return;
-    }
-
-    // If we had been observing another button, disconnect from that. If that
-    // element still exists in the DOM, click on it for good measure.
-    if (skipBtnObserver && observedSkipBtn) {
-      skipBtnObserver.disconnect();
-      triggerClick(observedSkipBtn);
-    }
-
-    // If this is the first skip button we have encountered, we will have to
-    // set up the observer first.
-    if (!skipBtnObserver) {
-      skipBtnObserver = new MutationObserver(function() {
-        if (!isElementVisible(observedSkipBtn)) {
-          return;
-        }
-
-        triggerClick(observedSkipBtn);
-        observedSkipBtn = undefined;
-        skipBtnObserver.disconnect();
-      });
-    }
-
-    // Since we will eventually be working on the button we need to have this
-    // reference stored.
-    observedSkipBtn = button;
-
-    // Note that we are actually observing the button's parent that has the
-    // display attribute, as the skip button's visibilty is controlled by its
-    // parent.
-    skipBtnObserver.observe(parentWithDisplayStyle, { attributes: true });
-  }
-
-  /**
-  * Loops over all the buttons that need to be clicked and triggers the click
-  * even on those buttons.
-  */
-  function checkAndClickButtons() {
-    existingButtons(skipButtonClassNameList).forEach(button => {
-      // We want to make sure that we are only pressing the skip button when it
-      // is visible on the screen, so that it is like an actual user is pressing
-      // it. This also gives a user time to not-skip the ad in the future.
-      if (!isElementVisible(button)) {
-        triggerClickWhenVisible(button);
-        
-        return;
-      } 
-
-      triggerClick(button);
-    })
-  }
-
-  /**
-  * We check if an ad is visible, and if it is, we mute it; otherwise, the video will be unmuted.
-  */
-  function checkAndMuteAd() {
-    var adDiv = document.querySelector('div.video-ads');
-    var muteButton = document.querySelector('button.ytp-mute-button');
-
-    if (isElementVisible(adDiv) === true && isMuted === false) {
-      triggerClick(muteButton);
-      isMuted = true;
-    } else if (isElementVisible(adDiv) === false && isMuted === true) {
-      triggerClick(muteButton);
-      isMuted = false;
-    }
-  }
-
-  /**
-  * Triggers a click event on the given DOM element.
-  * 
-  * This function is based on an answer here:
-  * https://stackoverflow.com/questions/809057/how-do-i-programmatically-click-on-an-element-in-javascript
-  * 
-  * @param {Element} el - The element on which to trigger the event
-  */
-  function triggerClick(el) {
-    var clickEvent = new MouseEvent("click", {
-      "view": window,
-      "bubbles": true,
-      "cancelable": false
-    });
-
-    el.dispatchEvent(clickEvent); 
   }
 
   /**
@@ -173,9 +46,9 @@
       return false;
     }
 
-    var observer = new MutationObserver(function() {
-      checkAndClickButtons();
-      checkAndMuteAd();
+    var observer = new MutationObserver(() => {
+      console.log("Observer function called");
+      checkAndSkipAd();
     });
 
     observer.observe(ytdPlayer, { childList: true, subtree: true });
@@ -186,10 +59,9 @@
   }
 
   /**
-  * We have two implementations to check for the skip ad buttons: one is based on
+  * We have two implementations to check for ads: one is based on
   * MutationObserver, that is only triggered when the video-player is updated in
-  * the page; second is a simple poll that constantly checks for the existence of
-  * the skip ad buttons.
+  * the page; second is a simple poll that constantly checks for the existence of ads.
   * 
   * We first try to set up the mutation observer. It can sometimes fail even when the
   * browser supports it, if the video player has not yet been attached to the DOM.
@@ -200,16 +72,12 @@
     clearTimeout(timeoutId);
 
     if (initObserver()) {
-      // We can stop the polling as the observer is set up.
+      console.log("Observer attached successfully to the player");
       return;
     }
 
-    /**
-     * Starts the poll to see if any of the ad buttons are present in the page now.
-     * The interval of 2 seconds is arbitrary. I believe it is a good compromise.
-     */
     timeoutId = setTimeout(function() {
-      checkAndClickButtons();
+      checkAndSkipAd();
 
       initTimeout();
     }, 2000);
